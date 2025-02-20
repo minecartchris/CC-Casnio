bf7d957ea37aec97d7b7c07b1eb51d8bb2476ff5
minecartchris <130516132+minecartchris@users.noreply.github.com>
2025-02-20 11:44:35 -0600

    Update Casino_install.lua

--- Casino_install.lua
+++ Casino_install.lua
@@ -1,6 +1,16 @@
 -- This will install the selected program
 print("Are you installing a Slot or ATM?")
 local A = io.read()
+local i = 5
 if A == "Slot" then
- shell.run("wget ")
+ shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/Slot.lua startup.lua")
+elseif A == "ATM"
+ shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/ATM.lua startup.lua")
 end
+
+print("Please remove installation medium")
+while i > 0 do
+ print("reboot in ",, i ,," seconds")
+ i = i - 1
+end
+shell.run("reboot")
