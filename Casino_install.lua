bb16f6bd872789b6bb3da1c150eaed62406ead1c
minecartchris <130516132+minecartchris@users.noreply.github.com>
2025-02-20 11:38:46 -0600

    Update Casino_install.lua

--- Casino_install.lua
+++ Casino_install.lua
@@ -1,1 +1,6 @@
--- this will install the selected prograam
+-- This will install the selected program
+print("Are you installing a Slot or ATM?")
+local A = io.read()
+if A == "Slot" then
+ shell.run("wget ")
+end
