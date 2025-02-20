1e856c44d8726aaeb1ca8c9b288ae39fe019a323
minecartchris <130516132+minecartchris@users.noreply.github.com>
2025-02-20 13:43:52 -0600

    Update Casino_install.lua

--- Casino_install.lua
+++ Casino_install.lua
@@ -1,4 +1,31 @@
 -- This will install the selected program
+if fs.exists("startup.lua") then
+ print("do you want to update existing file?")
+ local A = io.read()
+ if A == "yes" or A == "Yes" then
+  local soft = fs.open("startup.lua", "r")
+  local data = soft.readLine()
+  soft.close()
+  print("file read")
+  if data == "--ATM" then
+    fs.delete("startup.lua")
+    shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/ATM.lua startup.lua")
+  elseif data == "--Slot" then
+    fs.delete("startup.lua")
+    shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/Slot.lua startup.lua")
+  end
+
+  local i = 5
+  print("Please remove Update medium")
+  while i > 0 do
+   print("reboot in ".. i .." seconds")
+   sleep(1)
+   i = i - 1
+  end
+  shell.run("reboot")
+ end
+end
+
 print("Are you installing a Slot or ATM?")
 local A = io.read()
 local i = 5
