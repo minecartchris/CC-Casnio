f0b947ea4f67500ea6163e9219e30266825cf30b
Astronand <168994369+ASTRONAND@users.noreply.github.com>
2025-02-22 21:51:06 -0500

    e

--- Casino_install.lua
+++ Casino_install.lua
@@ -1,49 +1,49 @@
 -- This will install the selected program
 if fs.exists("startup.lua") then
- print("do you want to update existing file?")
- local A = io.read()
- if A == "yes" or A == "Yes" then
-  local soft = fs.open("startup.lua", "r")
-  local data = soft.readLine()
-  soft.close()
-  print("file read")
-  if data == "--ATM" then
-    fs.delete("startup.lua")
-    shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/ATM.lua startup.lua")
-  elseif data == "--Slot" then
-    fs.delete("startup.lua")
-    shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/Slot.lua startup.lua")
-  elseif data == "--tic" then
-   fs.delete("startup.lua")
-   shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/ticTackToe.lua startup.lua")
-  end
-
-  local i = 5
-  print("Please remove Update medium")
-  while i > 0 do
-   print("reboot in ".. i .." seconds")
-   sleep(1)
-   i = i - 1
-  end
-  shell.run("reboot")
- end
+    print("do you want to update existing file?")
+    local A = io.read()
+    if A == "yes" or A == "Yes" then
+        local soft = fs.open("startup.lua", "r")
+        local data = soft.readLine()
+        soft.close()
+        print("file read")
+        if data == "--ATM" then
+            fs.delete("startup.lua")
+            shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/ATM.lua startup.lua")
+        elseif data == "--Slot" then
+            fs.delete("startup.lua")
+            shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/Slot.lua startup.lua")
+        elseif data == "--tic" then
+            fs.delete("startup.lua")
+            shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/ticTackToe.lua startup.lua")
+        end
+      
+        local i = 5
+        print("Please remove Update medium")
+        while i > 0 do
+            print("reboot in ".. i .." seconds")
+            sleep(1)
+            i = i - 1
+        end
+        shell.run("reboot")
+    end
 end
 
 print("Are you installing a Slot, ATM or tic?")
 local A = io.read()
 local i = 5
 if A == "Slot" then
- shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/Slot.lua startup.lua")
+    shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/Slot.lua startup.lua")
 elseif A == "ATM" then
- shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/ATM.lua startup.lua")
+    shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/ATM.lua startup.lua")
 elseif A == "tic" then
- shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/ticTackToe.lua startup.lua")
+    shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/ticTackToe.lua startup.lua")
 end
 
 print("Please remove installation medium")
 while i > 0 do
- print("reboot in ".. i .." seconds")
- sleep(1)
- i = i - 1
+    print("reboot in ".. i .." seconds")
+    sleep(1)
+    i = i - 1
 end
 shell.run("reboot")
