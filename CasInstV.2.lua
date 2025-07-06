2847cb037a17b2bb7e27a4d9188fc9e17d3fa7aa
juryteacher67 <129633830+juryteacher67@users.noreply.github.com>
2025-07-06 04:04:13 -0500

    Update CasInstV.2.lua

--- CasInstV.2.lua
+++ CasInstV.2.lua
@@ -37,5 +37,23 @@ while true do
         else
             print("Invalid Input")
         end
+    else
+        print("Type Slot, ATM, tic, black jack to install game")
+        local B=io.read()
+        if B == "Slot" then
+            shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/Slot.lua startup.lua")
+            break
+        elseif B == "ATM" then
+            shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/ATM.lua startup.lua")
+            break
+        elseif B == "tic" then
+            shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/ticTackToe.lua startup.lua")
+            break
+        elseif B == "black jack" then
+            shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/BlackJack.lua startup.lua")
+            break
+        else
+            print("Invalid Input")
+        end
     end
 end
