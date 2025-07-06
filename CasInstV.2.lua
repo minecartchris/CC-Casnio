0a5b3dd4032abca9eacd5b40769276d76fb46968
juryteacher67 <129633830+juryteacher67@users.noreply.github.com>
2025-07-06 03:42:40 -0500

    Update CasInstV.2.lua
    Testing

--- CasInstV.2.lua
+++ CasInstV.2.lua
@@ -1,54 +1,33 @@
--- This will install the selected program
-if fs.exists("startup.lua") then
-    print("do you want to update existing file?")
-    local A = io.read()
-    if A == "yes" or A == "Yes" then
-        local soft = fs.open("startup.lua", "r")
-        local data = soft.readLine()
-        soft.close()
-        print("file read")
-        if data == "--ATM" then
-            fs.delete("startup.lua")
-            shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/ATM.lua startup.lua")
-        elseif data == "--Slot" then
-            fs.delete("startup.lua")
-            shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/Slot.lua startup.lua")
-        elseif data == "--tic" then
-            fs.delete("startup.lua")
-            shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/ticTackToe.lua startup.lua")
-        elseif data == "--black" then
-            fs.delete("startup.lua")
-            shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/BlackJack.lua startup.lua")
+while true do 
+    if fs.exist("startup.lua") then 
+        print("Would you like to Update the Current Startup File")
+        local Ans=io.read()
+        if(Ans=="Yes" or Ans=="yes" or Ans=="y" or Ans=="Y" then 
+            if data == "--ATM" then
+                fs.delete("startup.lua")
+                shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/ATM.lua startup.lua")
+            elseif data == "--Slot" then
+                fs.delete("startup.lua")
+                shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/Slot.lua startup.lua")
+            elseif data == "--tic" then
+                fs.delete("startup.lua")
+                shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/ticTackToe.lua startup.lua")
+            elseif data == "--black" then
+                fs.delete("startup.lua")
+                shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/BlackJack.lua startup.lua")
+            end
+        elseif (Ans=="No" or Ans=="no" or Ans=="N" or Ans=="n") then
+            if A == "Slot" then
+                shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/Slot.lua startup.lua")
+            elseif A == "ATM" then
+                shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/ATM.lua startup.lua")
+            elseif A == "tic" then
+                shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/ticTackToe.lua startup.lua")
+            elseif A == "black jack" then
+                shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/BlackJack.lua startup.lua")
+            end
+        else
+            print("Invalid Input")
         end
-      
-        local i = 5
-        print("Please remove Update medium")
-        while i > 0 do
-            print("reboot in ".. i .." seconds")
-            sleep(1)
-            i = i - 1
-        end
-        shell.run("reboot")
-    elseif(A=="No" or A=="no") then
-      print("Are you installing a Slot, ATM, black jack, or tictactoe?")
-    local A = io.read()
-    local i = 5
-    if A == "Slot" then
-      shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/Slot.lua startup.lua")
-    elseif A == "ATM" then
-      shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/ATM.lua startup.lua")
-    elseif A == "tic" then
-      shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/ticTackToe.lua startup.lua")
-    elseif A == "black jack" then
-      shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Casnio/refs/heads/main/prog/BlackJack.lua startup.lua")
     end
-
-    print("Please remove installation medium")
-    while i > 0 do
-      print("reboot in ".. i .." seconds")
-      sleep(1)
-      i = i - 1
-    end
-    shell.run("reboot")
-  end
-
+end
